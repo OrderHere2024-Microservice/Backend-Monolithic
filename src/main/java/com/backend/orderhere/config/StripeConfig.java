07bd118f43cb1bf53e29ec6ae3426073bf5c4f05
SteveGuoh <hghandle@outlook.com>
2024-09-08 19:55:45 +1000

    feature: place stripe key inside ssm parameter store

--- src/main/java/com/backend/orderhere/config/StripeConfig.java
+++ src/main/java/com/backend/orderhere/config/StripeConfig.java
@@ -1,11 +1,33 @@
 package com.backend.orderhere.config;
+
 import com.stripe.Stripe;
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ssm.SsmClient;
+import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
+import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
 
 @Configuration
 public class StripeConfig {
-    static {
-        Stripe.apiKey = "sk_test_51O79acCO47pkDdZVXGekJHrDVXbHAcbzaZzoNZ3CbMYSkPx8TcjKuybHND5HZ5vu14iJYfIAZCeEoUSmxmOqSOCW00SM8E2HZ2";
+
+    @PostConstruct
+    public void init() {
+        SsmClient ssmClient = SsmClient.builder().region(Region.AP_SOUTHEAST_2).build();
+
+        GetParameterRequest parameterRequest = GetParameterRequest.builder().name("/config/orderhere-monolithic/development/stripe/api-key").withDecryption(true).build();
+
+        GetParameterResponse parameterResponse = ssmClient.getParameter(parameterRequest);
+
+        String stripeApiKey = parameterResponse.parameter().value();
+
+        System.out.println("Stripe API Key: " + stripeApiKey);
+
+        if (stripeApiKey == null || stripeApiKey.isEmpty()) {
+            Stripe.apiKey = "placeholder";
+            // throw new IllegalArgumentException("Stripe API Key not found in Parameter Store");
+        }
+
+        Stripe.apiKey = stripeApiKey;
     }
 }
