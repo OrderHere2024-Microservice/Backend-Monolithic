cf42ab0f3dacaaa808bf346953f393f0e033a7f4
SteveGuoh <hghandle@outlook.com>
2024-09-10 20:35:14 +1000

    feature: minio local is working

--- src/main/java/com/backend/orderhere/service/storageService/S3StorageService.java
+++ src/main/java/com/backend/orderhere/service/storageService/S3StorageService.java
@@ -1,71 +1,64 @@
 package com.backend.orderhere.service.storageService;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
 import java.util.UUID;
 
 @Service
 @Profile("!local")
 public class S3StorageService implements StorageService {
 
-    private final AmazonS3 s3client;
+    private final S3Client s3Client;
 
-    public S3StorageService(@Value("${aws.accessKey}") String accessKey,
-                            @Value("${aws.secretKey}") String secretKey,
-                            @Value("${aws.region}") String region) {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        this.s3client = AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(Regions.fromName(region))
+    public S3StorageService() {
+        this.s3Client = S3Client.builder()
+                .credentialsProvider(ProfileCredentialsProvider.create())
+                .endpointOverride(URI.create("https://s3.amazonaws.com"))
+                .region(Region.AP_SOUTHEAST_2)  // Define your region
                 .build();
     }
 
     @Override
     public String uploadFile(MultipartFile file, String bucketName) throws IOException {
-        String originalFileName = file.getOriginalFilename();
-        String uniqueFileName = UUID.randomUUID() + "-" + originalFileName;
+        String originalName = file.getOriginalFilename();
+        String uniqueFileName = UUID.randomUUID() + "-" + originalName;
 
-        // Convert MultipartFile to File
-        File convertedFile = convertMultipartFileToFile(file);
-        s3client.putObject(bucketName, uniqueFileName, convertedFile);
+        try (InputStream inputStream = file.getInputStream()) {
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(uniqueFileName)
+                    .contentType("image/jpeg")
+                    .build();
 
-        // Clean up temporary file after upload
-        convertedFile.delete();
+            s3Client.putObject(putObjectRequest, software.amazon.awssdk.core.sync.RequestBody.fromInputStream(inputStream, file.getSize()));
 
-        return s3client.getUrl(bucketName, uniqueFileName).toString();
-    }
-
-    @Override
-    public void deleteFile(String bucketName, String fileName) {
-        try {
-            s3client.deleteObject(bucketName, fileName);
-            System.out.println("File deleted successfully.");
-        } catch (AmazonServiceException e) {
-            throw new RuntimeException("Failed to delete file from S3. " + e.getErrorMessage(), e);
-        } catch (AmazonClientException e) {
-            throw new RuntimeException("Client encountered an error while deleting the file. " + e.getMessage(), e);
+            return s3Client.utilities().getUrl(GetUrlRequest.builder().bucket(bucketName).key(uniqueFileName).build()).toString();
         }
     }
 
-    private File convertMultipartFileToFile(MultipartFile file) throws IOException {
-        File convFile = new File(file.getOriginalFilename());
-        FileOutputStream fos = new FileOutputStream(convFile);
-        fos.write(file.getBytes());
-        fos.close();
-        return convFile;
+    @Override
+    public void deleteFile(String bucketName, String imageUrl) throws Exception {
+        try {
+            String fileName = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(fileName)
+                    .build();
+
+            s3Client.deleteObject(deleteObjectRequest);
+        } catch (S3Exception e) {
+            throw new Exception("Error occurred while trying to delete file: " + imageUrl, e);
+        }
     }
 }
