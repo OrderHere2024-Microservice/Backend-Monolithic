b31bd3fe31f920f9d827acdec8a9fca32d505e33
SteveGuoh <hghandle@outlook.com>
2024-09-22 00:10:32 +1000

    Add text

--- src/main/java/com/backend/orderhere/exception/GlobalExceptionHandler.java
+++ src/main/java/com/backend/orderhere/exception/GlobalExceptionHandler.java
@@ -38,7 +38,7 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(Exception.class)
   public ResponseEntity<String> handleGenericException(Exception ex) {
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body("Something went wrong");
+        .body("Something went wrong: " + ex.getMessage());
   }
 
   @ExceptionHandler(MethodArgumentTypeMismatchException.class)
